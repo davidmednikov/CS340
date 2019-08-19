af75b7fba10143ec874c1ac7d84b4a4df4cc65c2
David Mednikov <davemednikov@gmail.com>
2019-08-18 23:28:28 -0700

    Revert "Flight Data"
    This reverts commit 70f1691331cc98febbcb282da714971e97eacf05.

--- data/csv/parser.js
+++
@@ -1,7 +0,0 @@
-fs = require('fs')
-fs.readFile('/Users/dmednikov/Desktop/School/OSU/340/project/data/csv/flights.csv', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data);
-});
